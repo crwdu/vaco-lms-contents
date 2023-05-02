9b01f0bd8bef51e9973cf2f67e7ea7369e3f4174
jett <jett@vanillacoding.co>
2023-05-02 11:30:51 +0900

    Add custom user hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "../styles/globals.css";
+import useUser from "../hooks/useUser";
 
 export default function Nextra({ Component, pageProps }) {
+  const { isLoggedIn } = useUser();
+
+  if (!isLoggedIn) {
+    return <></>;
+  }
+
   return <Component {...pageProps} />;
 }
