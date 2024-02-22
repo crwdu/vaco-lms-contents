2dbee80fc083665fb1ce783abef3e02bff77ffc7
dmhuh <dami@vanillacoding.co>
2024-02-22 18:11:34 +0900

    Add sending test run data feature

--- components/CustomSandpack/index.jsx
+++ components/CustomSandpack/index.jsx
@@ -1,12 +1,37 @@
+import { useEffect } from "react";
 import {
   SandpackProvider,
   SandpackCodeEditor,
   SandpackTests,
   SandpackLayout,
+  useSandpack,
 } from "@codesandbox/sandpack-react";
 import { autocompletion, completionKeymap } from "@codemirror/autocomplete";
+import useEventSender from "../../hooks/useEventSender";
 
-export default function Sandpack({ files }) {
+const ListenerIframeMessage = () => {
+  const eventSender = useEventSender();
+  const { listen } = useSandpack();
+
+  useEffect(() => {
+    const stopListening = listen((msg) => {
+      const { type, event } = msg;
+
+      if (type === "test" && event === "test_end") {
+        const { test } = msg;
+        const { name: testName, status: result } = test;
+
+        eventSender.send("run_test", [{ testName, result }]);
+      }
+    });
+
+    return () => {
+      stopListening();
+    };
+  }, [listen]);
+};
+
+export default function CustomSandpack({ files, main = "/app.test.js" }) {
   return (
     <SandpackProvider
       files={files}
@@ -16,12 +41,13 @@ export default function Sandpack({ files }) {
       showConsoleButton={true}
       customSetup={{
         entry: "/app.js",
-        main: "/app.test.js",
+        main,
         environment: "parcel",
         mode: "tests",
       }}
     >
       <SandpackLayout>
+        <ListenerIframeMessage />
         <SandpackCodeEditor
           showLineNumbers
           showTabs={false}
