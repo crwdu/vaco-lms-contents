bc8ffc18f87af7186fc285fede54867956961ac7
dmhuh <dami@vanillacoding.co>
2024-04-05 11:22:51 +0900

    fix: Update course enrollment validation logic

--- hooks/useUser.js
+++ hooks/useUser.js
@@ -1,7 +1,10 @@
+import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 
 export default function useUser() {
   const [isLoggedIn, setLogin] = useState(false);
+  const router = useRouter();
+  const course_id = router.basePath;
 
   useEffect(() => {
     async function getAuth() {
@@ -14,15 +17,13 @@ export default function useUser() {
       }
 
       const { result } = await response.json();
-      const { user, courses } = result.data;
+      const { courses: userCourses } = result.data;
 
-      // NOTE: 임시 처리, /api/auth 자체를 수정하여 다른 코스에도 대응할 수 있도록 변경 필요
-      const js101 = courses.find((course) => course.title === "js101");
-      const userJs101 = user.courses.find(
-        (userCourse) => userCourse.course_id === js101.id,
+      const courseEnrolled = !!userCourses.find(
+        (course) => `/${course.titleSlug}` === course_id,
       );
 
-      if (!userJs101) {
+      if (!courseEnrolled) {
         alert("코스를 결제해야 이용할 수 있는 기능입니다.");
         window.location = `${process.env.NEXT_PUBLIC_URI}`;
         return;
