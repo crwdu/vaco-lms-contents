7c2b7235f0c2ffc21187f1784daf65204884d827
dmhuh <dami@vanillacoding.co>
2024-04-16 20:03:22 +0900

    feat: Temporarily disable authorization checks

--- hooks/useUser.js
+++ hooks/useUser.js
@@ -17,17 +17,18 @@ export default function useUser() {
       }
 
       const { result } = await response.json();
-      const { courses: userCourses } = result.data;
+      // NOTE: (임시) 권한 체크 로직 해제
+      // const { courses: userCourses } = result.data;
 
-      const courseEnrolled = !!userCourses.find(
-        (course) => `/${course.titleSlug}` === course_id,
-      );
+      // const courseEnrolled = !!userCourses.find(
+      //   (course) => `/${course.titleSlug}` === course_id,
+      // );
 
-      if (!courseEnrolled) {
-        alert("코스를 결제해야 이용할 수 있는 기능입니다.");
-        window.location = `${process.env.NEXT_PUBLIC_URI}`;
-        return;
-      }
+      // if (!courseEnrolled) {
+      //   alert("코스를 결제해야 이용할 수 있는 기능입니다.");
+      //   window.location = `${process.env.NEXT_PUBLIC_URI}`;
+      //   return;
+      // }
 
       if (result.ok) {
         setLogin(true);
